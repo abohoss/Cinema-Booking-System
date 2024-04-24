3dbf29cf597b16716de332ad6299b1409a50e833
Yehia Sakr <yehiasakr4676@gmail.com>
2024-04-24 18:10:26 +0200

    implement addMovie and listMovies in Movie Class

--- Movie.py
+++ Movie.py
@@ -1,33 +1,39 @@
 import pyodbc
+
 class Movie:
-    def __init__(self,Name,Description,Genre,Cast):
-        self.Name = Name 
+    def __init__(self, Name, Description, Genre, EmpId, Cast):
+        self.Name = Name
         self.Description = Description
         self.Genre = Genre
+        self.EmpId = EmpId
         self.Cast = Cast
 
+def add_movie(movie):
+        cursor.execute("EXEC AddMovie ?, ?, ?, ?, ?", movie.Name, movie.Description, movie.Genre, movie.EmpId, movie.Cast)
+        conn.commit()
+
+def list_movies():
+        cursor.execute("EXEC ListMovies")
+
+        rows = cursor.fetchall()
+        for row in rows:
+            print("Movie Name:", row.Name)
+            print("Description:", row.Description)
+            print("Genre:", row.Genre)
+            print("Cast Members:", row.Actors)
+            print("-" * 50)
+    
+
+
+#---------------------------------------------------------------------------------------------------------------------------#
 # Connect to the SQL Server database
 conn = pyodbc.connect('Driver={SQL Server};Server={DESKTOP-Q2Q9TUS};Database={Cinema}')
-
-# Create a cursor object to execute SQL statements
 cursor = conn.cursor()
 
-# Create the Movie table
-create_table_query = '''
-    CREATE TABLE Movie (
-        
-        Name VARCHAR(100) PRIMARY KEY,
-        Description VARCHAR(255),
-        Genre VARCHAR(50),
-        Employee_Id INT,
-        FOREIGN KEY (Employee_Id) REFERENCES Employee(Emp_Id)
-    )
-'''
+# movie = Movie(Name='The Avengers', Description='Superhero movie', Genre='Action', EmpId=1, Cast='Robert Downey Jr., Chris Evans, Scarlett Johansson')
+# add_movie(movie)
 
-
-cursor.execute(create_table_query)
-conn.commit()
+list_movies()
 
 # Close the connection
-conn.close()        
-        
+conn.close()
