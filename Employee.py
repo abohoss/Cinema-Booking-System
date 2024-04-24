096c26e52659aacba174b0416be4272df562967c
Yehia Sakr <yehiasakr4676@gmail.com>
2024-04-24 18:10:03 +0200

    implement create employee in employee class

--- Employee.py
+++ Employee.py
@@ -1,38 +1,33 @@
 import pyodbc
+
 class Employee:
-    def __init__(self,Fname,Lname,Emp_id,Salary,Role,Street_Name,Building_no,Apartment_no):
-        self.Fname=Fname
-        self.Lname=Lname
-        self.Emp_id=Emp_id
-        self.Salary=Salary
-        self.Role=Role
-        self.Street_Name=Street_Name
-        self.Building_no=Building_no
-        self.Apartment_no=Apartment_no
+    def __init__(self, Fname, Lname, Emp_id, Salary, Role, Street_Name, Building_no, Apartment_no):
+        self.Fname = Fname
+        self.Lname = Lname
+        self.Emp_id = Emp_id
+        self.Salary = Salary
+        self.Role = Role
+        self.Street_Name = Street_Name
+        self.Building_no = Building_no
+        self.Apartment_no = Apartment_no
+#------------------------------------------------------------------------------------------------------------
+def create_employee_account(employee):
+    
+    cursor.execute("EXEC CreateEmployeeAccount ?, ?, ?, ?, ?, ?, ?, ?",
+                   employee.Emp_id, employee.Fname, employee.Lname, employee.Salary,
+                   employee.Role, employee.Street_Name, employee.Building_no, employee.Apartment_no)
+    cursor.commit()
+
+
 # Connect to the SQL Server database
 conn = pyodbc.connect('Driver={SQL Server};Server={DESKTOP-Q2Q9TUS};Database={Cinema}')
-
-# Create a cursor object to execute SQL statements
 cursor = conn.cursor()
 
-# Create the Employee table
-create_table_query = '''
-        CREATE TABLE Employee (
-            Emp_id INT PRIMARY KEY,
-            firstName VARCHAR(50),
-            lastName VARCHAR(50),
-            Salary DECIMAL(10, 2),
-            Role VARCHAR(50),
-            streetName VARCHAR(100),
-            buildingNumber INT,
-            apartmentNumber INT
-    )
-'''
+# employee = Employee(Fname='John', Lname='Doe', Emp_id=1, Salary=5000.00, Role='Manager',
+#                     Street_Name='123 Main St', Building_no=1, Apartment_no=2)
 
+# create_employee_account(employee)
 
-cursor.execute(create_table_query)
-conn.commit()
 
 # Close the connection
-conn.close()        
-        
+conn.close()
