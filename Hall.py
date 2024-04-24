50ebefc3eb76c398968657dec75c31484b44f134
Yehia Sakr <yehiasakr4676@gmail.com>
2024-04-24 18:09:07 +0200

    remove db declaration in Hall class

--- Hall.py
+++ Hall.py
@@ -5,22 +5,3 @@ class Hall:
         self.screen_type =screen_type
         self.seats =seats
         
-# Connect to the SQL Server database
-conn = pyodbc.connect('Driver={SQL Server};Server={DESKTOP-Q2Q9TUS};Database={Cinema}')
-
-# Create a cursor object to execute SQL statements
-cursor = conn.cursor()
-
-# Create the Hall table
-create_table_query = '''
-    CREATE TABLE Hall (
-        Hall_Num INT PRIMARY KEY,
-        Screen_Type VARCHAR(50)
-    )
-'''
-
-cursor.execute(create_table_query)
-conn.commit()
-
-# Close the connection
-conn.close()
