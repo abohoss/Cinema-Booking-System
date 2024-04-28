08a00d1da0dd304038a6ba10f1fa44508d7043f7
Mark Saleh <mark-saleh@outlook.com>
2024-04-28 22:21:03 +0300

    Update AddMovie and ListMovies procedures to return and receive image_url

--- Procedures.sql
+++ Procedures.sql
@@ -93,6 +93,7 @@ CREATE PROCEDURE AddMovie
     @Description VARCHAR(255),
     @Genre VARCHAR(50),
     @EmployeeId INT,
+	@image_url VARCHAR(255),
     @Cast NVARCHAR(MAX)
 AS
 BEGIN
@@ -105,8 +106,8 @@ BEGIN
     END;
 
     -- Insert the movie into the Movie table
-    INSERT INTO Movie (Name, Description, Genre, Employee_Id)
-    VALUES (@Name, @Description, @Genre, @EmployeeId);
+    INSERT INTO Movie (Name, Description, Genre, Employee_Id, image_url)
+    VALUES (@Name, @Description, @Genre, @EmployeeId, @image_url);
 
     -- Insert the cast members into the Cast table
     INSERT INTO Cast (MovieName, Actors)
@@ -119,11 +120,11 @@ go
 CREATE PROCEDURE ListMovies
 AS
 BEGIN
-    SELECT M.Name, M.Description, M.Genre,
+    SELECT M.Name, M.Description, M.Genre, M.image_url,
         STRING_AGG(C.Actors, ', ') AS Actors
     FROM Movie M
     JOIN Cast C ON M.Name = C.MovieName
-    GROUP BY M.Name, M.Description, M.Genre;
+    GROUP BY M.Name, M.Description, M.Genre, M.image_url;
 END;
 go
 
@@ -170,7 +171,7 @@ BEGIN
     DELETE FROM Movie WHERE Name = @name;
 END;
 Exec DeleteMovie
-@name = 'Se7en';
+@name = 'Interstellar';
 go
 CREATE PROCEDURE selectMovie
     @name VARCHAR(100)
